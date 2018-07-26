f3e3f1fa79cf4112a2a5c4f2293261c479ad7ff5
Andy Leenders <andy.leenders@polteq.com>
2018-07-26 15:12:49 +0200

    beforeAfterChapter

--- src/test/java/exercises/TestShopScenario.java
+++ src/test/java/exercises/TestShopScenario.java
@@ -1,22 +1,37 @@
 package exercises;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.assertj.core.api.Assertions;
 
 import java.util.concurrent.TimeUnit;
 
-public class FirstSeleniumTest extends TestShopScenario {
+public class TestShopScenario {
     int ascii = 0x2713;
     String check = Character.toString((char)ascii);
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        System.out.println("Tested steps:");
+        driver.get("https://techblog.polteq.com/testshop/index.php");
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();System.out.println("-Browser closing succeed, test passed"+ check);
+    }
     @Test
     public void logInSuccesful() throws InterruptedException{
-        //ChromeDriverManager.getInstance().setup();
-        //WebDriver driver = new ChromeDriver();
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+        System.out.println("Tested steps:");
         driver.get("https://techblog.polteq.com/testshop/index.php");
         driver.manage().window().maximize();System.out.println("-Page loaded correctly" + check);
         driver.findElement(By.className("login")).click();System.out.println("-Redirect to loginpage succeeded"+ check);
@@ -29,9 +44,8 @@ public class FirstSeleniumTest extends TestShopScenario {
         Assertions.assertThat(assertBoolean).as("Adressenlijst is weergegeven").isTrue();System.out.println("-Adressenlijst weergegeven"+ check);
 
         String assertText = driver.findElement(By.className("logout")).getText();
-        Assertions.assertThat(assertText).as("Tekst bevat geen Sign Out").contains("Sign out"); System.out.println("-Sign Out button aanwezig \n-Succesfully logged in because presence 'Sign Out' "+ check);
+        Assertions.assertThat(assertText).as("Tekst bevat geen Sign Out").contains("Sign out"); System.out.println("-Sign Out button aanwezig /n -Succesfully logged in because presence 'Sign Out' "+ check);
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-            driver.quit();
 
     }
 }
